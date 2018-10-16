f0cb51afc220f4a592f7316a1a5f2ca09670c082
benedictbutton <benedictdaly@hotmail.com>
2018-10-16 16:42:08 -0400

    sign-in/out outline

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
+import logo from "./logo.svg";
+import "./App.css";
+
+//App Components
+import HomeContainer from "./containers/HomeContainer";
+import SignInContainer from "./containers/SignInContainer";
+import SignUpContainer from "./containers/SignUpContainer";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <Switch>
+            <Route exact path="/" component={HomeContainer} />
+            <Route exact path="/sign-up" component={SignUpContainer} />
+            <Route exact path="/login" component={SignInContainer} />
+          </Switch>
         </header>
       </div>
     );
