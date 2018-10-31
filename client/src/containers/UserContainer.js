44dd0a2483bcec4ec80ae0defb93be1b70fa34d1
benedictbutton <benedictdaly@hotmail.com>
2018-10-31 18:27:21 -0400

    user containter for future

--- client/src/containers/UserContainer.js
+++ client/src/containers/UserContainer.js
@@ -4,39 +4,17 @@ import { connect } from "react-redux";
 import { doShowUser, getUser } from "../redux/actions/userActions";
 import User from "../components/User";
 //
-class UserContainer extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  // componentDidMount() {
-  // console.log(this.state.user);
-  // }
-  //
-  // componentDidUpdate(prevProps) {
-  //   if (this.props.user.id !== prevProps.user.id) {
-  //     this.fetchUser(this.props.user.id);
-  //   }
-  // }
-
-  render() {
-    return <User {...this.props} />;
-  }
-}
+const UserContainer = props => {
+  return <User {...props} />;
+};
 
 const mapStateToProps = (state, props) => ({
   user: state.user
 });
 
-// const mapDispatchToProps = dispatch => ({
-//   getUser: () => dispatch(getUser)
-// });
-//
-// export default UserContainer;
-
 export default withRouter(
   connect(
     mapStateToProps,
-    { getUser: getUser }
+    undefined
   )(UserContainer)
 );
