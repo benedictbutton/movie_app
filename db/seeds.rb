813483db06d6a5b8bf758429cafcb68ad773e45f
benedictbutton <benedictdaly@hotmail.com>
2019-09-16 15:34:27 -0400

    admin privs

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,5 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-user = User.find_by(username: 'testuser3')
+user = User.find_by(username: 'testuser')
 user.admin!
