58f19071652a82a21f685028623129c08ea06808
benedictbutton <benedictdaly@hotmail.com>
2019-10-19 19:54:31 -0400

    cors issue

--- client/src/index.js
+++ client/src/index.js
@@ -7,7 +7,7 @@ import App from "./App";
 import { Provider } from "react-redux";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { store } from "./redux/store/configureStore";
-// import * as serviceWorker from "./serviceWorker";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <div>
@@ -24,4 +24,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-// serviceWorker.unregister();
+serviceWorker.unregister();
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://movies-scene.herokuapp.com','http://localhost:3000'
+    origins 'http://movies-scene.herokuapp.com','http://localhost:3000', 'http://movies-scene.com'
 
     resource '*',
       headers: :any,
