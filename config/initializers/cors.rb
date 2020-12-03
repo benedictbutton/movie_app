6aaf10614e13f54da58a9eead786df95b7a7357e
benedictbutton <benedictdaly@hotmail.com>
2020-12-02 21:24:05 -0500

    rack-cors expose

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -12,6 +12,6 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      expose: ['Access-Control-Request-Method']
+      expose: ['Access-Control-Request-Origin']
   end
 end
