9f63fc9378351b0c0f7677eab8f1d8f1ae48d9fe
benedict button <benedictdaly@hotmail.com>
2023-04-03 18:45:25 -0400

    update stack

--- client/src/containers/ActionIconsContainer.js
+++ client/src/containers/ActionIconsContainer.js
@@ -1,54 +1,54 @@
-import React from "react";
-import classNames from "classnames";
-import Grid from "@material-ui/core/Grid";
-import { withStyles } from "@material-ui/core/styles";
+import React from 'react'
+import classNames from 'classnames'
+import Grid from '@material-ui/core/Grid'
+import { withStyles } from '@material-ui/core/styles'
 
-export const styles = theme => ({
-  /* Styles applied to the root element. */
-  root: {
-    position: "absolute",
-    left: 0,
-    right: 0,
-    height: "48",
-    paddingBottom: theme.spacing.unit
-  },
-  /* Styles applied to the root element if `titlePosition="bottom"`. */
-  titlePositionBottom: {
-    background: "rgba(0, 0, 0, 0.5)",
-    bottom: 0
-  },
-  /* Styles applied to the root element if `titlePosition="top"`. */
-  titlePositionTop: {
-    background: "transparent",
-    top: 0,
-    justifyContent: "flex-end"
-  }
-});
+export const styles = (theme) => ({
+    /* Styles applied to the root element. */
+    root: {
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        height: '48',
+        paddingBottom: theme.spacing.unit,
+    },
+    /* Styles applied to the root element if `titlePosition="bottom"`.  */
+    titlePositionBottom: {
+        background: 'rgba(0, 0, 0, 0.5)',
+        bottom: 0,
+    },
+    /* Styles applied to the root element if `titlePosition="top"`. */
+    titlePositionTop: {
+        background: 'transparent',
+        top: 0,
+        justifyContent: 'flex-end',
+    },
+})
 
-const ActionIconsContainer = props => {
-  const { starsVisible, actionIcon, classes, titlePosition } = props;
+const ActionIconsContainer = (props) => {
+    const { starsVisible, actionIcon, classes, titlePosition } = props
 
-  const className = classNames(classes.root, {
-    [classes.titlePositionBottom]: titlePosition === "bottom",
-    [classes.titlePositionTop]: titlePosition === "top"
-  });
+    const className = classNames(classes.root, {
+        [classes.titlePositionBottom]: titlePosition === 'bottom',
+        [classes.titlePositionTop]: titlePosition === 'top',
+    })
 
-  return (
-    <div>
-      {actionIcon && starsVisible ? (
-        <Grid container className={className}>
-          {actionIcon}
-        </Grid>
-      ) : null}
-    </div>
-  );
-};
+    return (
+        <div>
+            {actionIcon && starsVisible ? (
+                <Grid container className={className}>
+                    {actionIcon}
+                </Grid>
+            ) : null}
+        </div>
+    )
+}
 
 ActionIconsContainer.defaultProps = {
-  actionPosition: "right",
-  titlePosition: "bottom"
-};
+    actionPosition: 'right',
+    titlePosition: 'bottom',
+}
 
-export default withStyles(styles, { name: "MuiGridListTileBar" })(
-  ActionIconsContainer
-);
+export default withStyles(styles, { name: 'MuiGridListTileBar' })(
+    ActionIconsContainer
+)
