feab70436ba1ab5af54d417f37fe18de34658b28
benedict button <benedictdaly@hotmail.com>
2023-05-17 20:11:43 -0400

    more cors config

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://movies-scene.herokuapp.com','http://localhost:3000', 'http://www.movies-scene.com'
+    origins 'http://movies-scene.herokuapp.com', 'http://movies-scene.herokuapp.com', 'http://localhost:3000', 'http://www.movies-scene.com', 'https://www.movies-scene.com'
 
     resource '*',
       headers: :any,
